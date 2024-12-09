a4de3b22ed3768b1dd3df06ae09b03ef69430900
aqsashah <aqsanor665@gmail.com>
2024-12-09 19:49:04 +0500

    Update Navbar.tsx

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       </div>
 
       {/* Center - Navigation Links */}
-      <div className="flex space-x-4 sm:space-x-2 lg:space-x-6 text-sm font-medium text-gray-800">
+      <div className="flex space-x-2 sm:space-x-2 lg:space-x-8 text-sm font-medium  text-gray-800 justify-center  xl:ml-20">
         <Link
           href="/products"
           className="hover:text-gray-500 hidden sm:block lg:inline-block"
@@ -31,13 +31,13 @@ const Navbar = () => {
         </Link>
         <Link
           href="/products"
-          className="hover:text-gray-500 text-xs sm:text-sm"
+          className="hover:text-gray-500 text-xs hidden sm:block sm:text-sm"
         >
           Men
         </Link>
         <Link
           href="/products"
-          className="hover:text-gray-500 text-xs sm:text-sm"
+          className="hover:text-gray-500 text-xs hidden sm:block sm:text-sm"
         >
           Women
         </Link>
@@ -51,7 +51,7 @@ const Navbar = () => {
           href="/products"
           className="hover:text-gray-500 text-xs sm:text-sm"
         >
-          Sale
+          Products
         </Link>
         <Link
           href="/products"
@@ -62,9 +62,16 @@ const Navbar = () => {
       </div>
 
       {/* Right Side - Icons */}
-      <div className="flex items-center space-x-3  sm:space-x-4">
-        {/* Search Icon */}
-        <FaSearch className="text-base sm:text-lg text-gray-500 cursor-pointer" />
+      <div className="flex items-center border rounded-full px-3 py-2 text-sm text-gray-700 bg-gray-100 xl:ml-56 lg:ml-40">
+  <FaSearch className="mr-2 text-gray-500 cursor-pointer" />
+  <input
+    type="text"
+    placeholder="Search"
+    className="bg-transparent outline-none w-16 sm:w-10 md:w-40 lg:w-44 xl:w-58 placeholder:hidden sm:placeholder:block"
+  />
+</div>
+
+
 
         {/* Wishlist */}
         <Link href="/Checkout">
@@ -75,7 +82,6 @@ const Navbar = () => {
         <Link href="/cart">
           <RiShoppingBagLine className="text-base sm:text-lg hover:text-gray-500 cursor-pointer" />
         </Link>
-      </div>
     </nav>
   );
 };
